5a80d94891c2f866efa3a287fe81d75a723374df
Felix-OYO <hoffefel000@alpinesd.org>
2024-12-18 10:19:59 -0700

    Update for_summation.py

--- for_summation.py
+++ for_summation.py
@@ -1,1 +1,8 @@
-#for Summation code here
+number = int(input(""))
+answer = 0
+
+for i in range(number):
+    # Add a one because the loop starts at 0
+    answer = answer + i + 1
+
+print(f"{answer}")
